49ff1aacb183fc4ac0778986078f5f0f155eae1f
2002goblue <beburger@umich.edu>
2026-09-22 21:43:47 -0400

    Update documentation and precision of checkout.py
    Update checkout.py to round to two decimal places when returning the total. Function documentation added.

--- checkout.py
+++ checkout.py
@@ -1,5 +1,7 @@
+# Input the item price and the percent tax to be added
+# The total price will be returned
 def checkout(price, taxPercent):
     total = (1+taxPercent)*price
-    return total
+    return round(total, 2)
 
 print(checkout(50, 0.12))
