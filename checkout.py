6838b8b2c485e9fb608ae75f40378a2916a095ba
2002goblue <beburger@umich.edu>
2026-09-22 22:10:36 -0400

    Fix total calculation in checkout.py
    Fix bug in the total after tax calculation due to misplaced parentheses in checkout.py.

--- checkout.py
+++ checkout.py
@@ -1,7 +1,7 @@
 # Input the item price and the percent tax to be added
 # The total price will be returned
 def checkout(price, taxPercent):
-    total = 1+(taxPercent*price)
+    total = (1+taxPercent)*price
     return round(total, 2)
 
 print(checkout(50, 0.12))
